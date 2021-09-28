6c70a7b96ee7192e66d7ea452ab88fad4050f177
Daniel H <60170554+DanWlker@users.noreply.github.com>
2021-09-28 15:21:56 +0000

    Reformat to into classes

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import discord
-import nacl
-import os
-from discord import FFmpegPCMAudio
-from youtube_dl import YoutubeDL
-from discord.ext import commands
 
+import os
+from discord.ext import commands
+from musicCog import musicCog
 
 client = commands.Bot(command_prefix="!")
 bot_token = os.environ['TOKEN']
@@ -13,33 +10,9 @@ bot_token = os.environ['TOKEN']
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.command(pass_context=True)
-async def play_youtube(ctx, youtube_url):
-  print("Play youtube command received")
+cogs = [musicCog]
 
-  YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-  FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-  channel = ctx.message.author.voice.channel
-  if(youtube_url.startswith('https://www.youtube.com/watch?v=')):
-    voice = await channel.connect()
-    if not voice.is_playing():
-      with YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(youtube_url, download=False)
-        URL = info['formats'][0]['url']
-        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-        voice.is_playing()
-    else:
-        await ctx.send("Already playing song")
-        return
-  else:
-    return 'URL_ERROR'
-
-@client.command(pass_context=True)
-async def disconnect(ctx):
-  if ctx.author.voice is None:
-    await ctx.send("Im not in a channel ")
-  else:
-    await ctx.voice_client.disconnect()
+for i in range(len(cogs)):
+  cogs[i].setup(client)
 
 client.run(bot_token)
---
+++ musicCog.py
@@ -0,0 +1,81 @@
+import nacl
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
+
+class musicCog(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+  def setup(client): 
+    client.add_cog(musicCog(client))
+
+  async def join(self, ctx):
+    if(ctx.author.voice is None): #if author is not in channel
+      await ctx.send("Please enter a voice channel before using bot")
+      return
+
+    voiceChannel = ctx.author.voice.channel
+
+    if(ctx.voice_client is None):
+      await voiceChannel.connect()
+      return
+
+    if(ctx.voice_client != voiceChannel): #no need to move if in same voice channel
+      await self.disconnect(ctx)
+      await self.join(ctx)
+      return
+
+    print("same channel")
+
+  @commands.command(pass_context=True)
+  async def disconnect(self, ctx):
+    await ctx.voice_client.disconnect()
+
+  @commands.command(pass_context=True)
+  async def play(self, ctx, youtube_url):
+    await self.join(ctx)
+    ctx.voice_client.stop()
+
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': True,}
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    voice = ctx.voice_client
+
+    with YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(youtube_url, download=False)
+        URL = info['formats'][0]['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+
+  @commands.command(pass_context=True)
+  async def pause(self, ctx):
+    await ctx.send("Music has been paused")
+    await ctx.voice_client.pause()
+  
+  @commands.command(pass_context=True)
+  async def resume(self, ctx):
+    await ctx.send("Music has been resumed")
+    await ctx.voice_client.resume()
+
+  @commands.command(pass_context=True)
+  async def move(self, ctx):
+    if(ctx.author.voice is None): #if author is not in channel
+      await ctx.send("Please enter a voice channel before using bot")
+      return
+    await ctx.voice_client.move_to(ctx.author.voice.channel)
+
+  @commands.command(pass_context=True)
+  async def musichelp(self, ctx):
+    str = \
+    """
+**!play <youtube link>**
+> Play song from youtube link
+**!pause**
+> Pause playback of current song
+**!resume**
+> Resume playback of current song
+**!move**
+> To move the bot to another channel without changing song
+**!disconnect**
+> Disconnect bot from channel
+    """
+    await ctx.send(str)
